bf46f3064ba9ad0f03c59d28d15830354ff269f0
Samantha Carow <samantha.carow@gmail.com>
2015-09-19 17:56:24 -0700

    small bug with flash; added initializer for @tweets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,18 @@
 class HomeController < ApplicationController
 
+  def initialize
+    @tweets = []
+  end
+
   def index
-    @tweets = [];
   end
 
   def get_tweets
-    # return error if no handle
     handle = params[:handle]
-    @tweets = []
     begin
       @tweets = $client.user_timeline(handle, count: 25)
     rescue Exception => e
-      flash[:error] = e.message
+      flash.now[:error] = e.message
     end
 
     render 'index'
