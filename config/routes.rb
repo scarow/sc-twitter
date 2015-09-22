b763f2e535f936cbfa1405a5162e6db778d5ccd0
Samantha Carow <samantha.carow@gmail.com>
2015-09-21 23:44:15 -0700

    more styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
-  post 'home/get_tweets'
-  root 'home#index'
+  get "home/index"
+  post "home/get_tweets"
+  root "home#index"
 end
